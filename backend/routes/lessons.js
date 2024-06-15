e80547654dc963bdda6a580b6cbfa01c820747cb
Jane Doe <raidho27@gmail.com>
2024-06-15 17:56:32 +0300

    verify auth

--- backend/routes/lessons.js
+++ backend/routes/lessons.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import {createLesson, getLessons, getOneLesson, deleteLesson, updateLesson} from '../controllers/lessonController.js'
 
+import { requireAuth } from '../middleware/requireAuth.js'
+
+// require auth for all lesson routes
 const router = express.Router()
+
+router.use(requireAuth)
 
 router.get('/', getLessons)
 
